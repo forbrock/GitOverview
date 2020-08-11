c91243cfb59a4fbbc3c6842d80ca84e73abba619
Dmitrii Barsuk <badgeon@gmail.com>
2020-08-11 23:23:26 +0300

    Updated pom.xml and created test class StringReverseTest

---
+++ src/test/java/StringReverseTest.java
@@ -0,0 +1,30 @@
+import net.example.stringreverse.StringReverse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringReverseTest {
+    private static Stream<Arguments> dataSet() {
+        return Stream.of(
+                Arguments.of("hello", "olleh"),
+                Arguments.of("Привет как дела?", "?алед как тевирП"),
+                Arguments.of("   ", "   "),
+                Arguments.of("o_O   ", "   O_o"),
+                Arguments.of("", ""));
+    }
+
+    @DisplayName("Test XOR reverse method")
+    @ParameterizedTest
+    @MethodSource("dataSet")
+    void testReverseXOR(String a, String b) {
+        StringReverse solution = new StringReverse();
+        String actual = solution.reverseXOR(a);
+        String expected = b;
+        assertEquals(expected, actual);
+    }
+}
